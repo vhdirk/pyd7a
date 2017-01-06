32fa3b0cecafe11d405d0471f74132d09df70533
Glenn Ergeerts <glenn.ergeerts@uantwerpen.be>
2017-01-06 07:28:09 +0100

    show file details in seperate pane

--- modem-webgui/static/views/files.js
+++ modem-webgui/static/views/files.js
@@ -3,6 +3,11 @@ define([
     "models/modem",
     "models/files",
 ],function(app, modem, files){
+    function showFileDetail(file){
+		console.log("show detail: " + file.file_id);
+		$$("file_data").parse({'file_data': file.data});
+    }
+
     var ui = {
         rows:[
             { view: "toolbar", css: "highlighted_header header1", height: 40, cols: [
@@ -15,13 +20,13 @@ define([
                         id:"file_list",
                         columns:[
                             {id:"file_id", header:"ID", sort:"int"},
-                            {id:"file_name", header:"Filename", fillspace:true},
-                            {id:"data", header:"data", fillspace:true}
+                            {id:"file_name", header:"Filename", fillspace:true}
                         ],
                         data:files.data,
                         on:{
                             'onItemClick':function(id){
                                modem.read_file(this.getItem(id).file_id);
+                                showFileDetail(this.getItem(id));
                             }
                         }
                     },
@@ -32,8 +37,19 @@ define([
         ]
     };
 
+    function onInit() {
+        // make sure changes to the file are updated in the file details pane, update the UI after details have been received.
+        // TODO check if we can get this through databinding, like it is used for the datatable.
+        files.data.attachEvent("onDataUpdate", function(id, obj){
+            showFileDetail(files.data.getItem(id));
+            return true;
+        });
+
+    }
+
 	return {
 		$ui: ui,
 		$menu: "top:menu",
+        $oninit: onInit()
 	};
 });
