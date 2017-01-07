1fc1b5536369a040c39ddda5a889f831e94d1ef2
Glenn Ergeerts <glenn.ergeerts@uantwerpen.be>
2017-01-07 15:16:32 +0100

    refactoring: moved parsing of known system files content to ReturnFileData operation

--- d7a/alp/action.py
+++ d7a/alp/action.py
@@ -4,23 +4,12 @@
 # class implementation of action parameters
 
 # D7A ALP Action 
-#
-# ALP Control   1 Byte
-#   b7    GROUP Group this action with the next one (see 11.4.3)
-#   b6    RESP  Return ALP Error Template
-#   b5-b0 OP    Operation describing the action
-# ALP Operand   N Bytes
-from bitstring import ConstBitStream
-
 from d7a.alp.operations.requests import ReadFileData
 from d7a.alp.operations.responses import ReturnFileData
 from d7a.support.schema           import Validatable, Types
 
 from d7a.alp.operations.operation import Operation
 from d7a.alp.operations.nop       import NoOperation
-from d7a.system_files.system_file_ids import SystemFileIds
-from d7a.system_files.system_files import SystemFiles
-
 
 class Action(Validatable):
 
@@ -45,22 +34,13 @@ class Action(Validatable):
   def __str__(self):
     if isinstance(self.operation, ReturnFileData):
       # when reading a known system files we output the parsed data
-      try:
-        systemfile_type = SystemFiles().get_all_system_files()[SystemFileIds(self.operand.offset.id)]
-        if systemfile_type is not None and systemfile_type.length == self.operand.length:
-          systemfile_content = systemfile_type.parse(ConstBitStream(bytearray(self.operand.data)))
-          return "Received {} content: {}".format(systemfile_type.__class__.__name__, systemfile_content)
-
-        return "Received {} partial ({} of {} bytes)".format(systemfile_type.__class__.__name__, self.operand.length,
-                                                               systemfile_type.length)
-      except:
-        # not a SystemFile
+      if self.operation.systemfile_type != None and self.operation.file_data_parsed != None:
+        return "Received {} content: {}".format(self.operation.systemfile_type.__class__.__name__,
+                                                self.operation.file_data_parsed)
+      else:
         return "Received file {} content: {}".format(self.operand.offset.id, self.operand.data)
 
     elif isinstance(self.operation, ReadFileData):
       return "Read file {}".format(self.operand.offset.id)
 
     return "{}".format(type(self.operation).__name__)
-
-  def __repr__(self):
-    return "op={}, operand={}({})".format(type(self.operation).__name__, type(self.operand).__name__, self.operand)
