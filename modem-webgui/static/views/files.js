19b9a5c51dea883a57ed511ce401c638d54b4fdc
Glenn Ergeerts <glenn.ergeerts@uantwerpen.be>
2017-01-19 12:03:28 +0100

    show file metadata

--- modem-webgui/static/views/files.js
+++ modem-webgui/static/views/files.js
@@ -5,8 +5,8 @@ define([
 ],function(app, modem, files){
     function showFileDetail(file){
 		console.log("show detail: " + file.file_id);
-		$$("file_details_title").parse({'file_name': file.file_name, 'file_id': file.file_id});
-
+        $$('file_metadata_form').setValues(file);
+        
         // dynamically load form based on filename
         var filename = file.file_name;
         if(filename.startsWith("ACCESS_PROFILE"))
@@ -43,10 +43,34 @@ define([
                         rows: [
                             {
                                 view: "toolbar" ,css: "highlighted_header header1", height: 40, cols: [
-                                    {id: "file_details_title", template: "File details - #file_name# (#file_id#)",
-                                        data: {"file_name": "", "file_id": ""}}
+                                    {id: "file_details_title", template: "File details"}
                                 ]
                             },
+                            {template: "File metadata", type: "section"},
+                            { view: "form", id: "file_metadata_form", complexData: true, elements: [
+                                {
+                                    view:"text",
+                                    label:"File name",
+                                    name:"file_name",
+                                    disabled:true,
+                                    labelWidth:300 // TODO should not be necessary
+                                },
+                                {
+                                    view:"text",
+                                    label:"File ID",
+                                    name:"file_id",
+                                    disabled:true,
+                                    labelWidth:300 // TODO should not be necessary
+                                },
+                                {
+                                    view:"text",
+                                    label:"File size",
+                                    name:"data.length",
+                                    disabled:true,
+                                    labelWidth:300 // TODO should not be necessary
+                                },
+                            ]},
+                            {template: "File contents", type: "section"},
                             { $subview: true }
                         ]
                     }
