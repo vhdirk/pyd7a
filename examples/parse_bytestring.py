3db19ff37249d0e1975feaf46f2596ef6d0f8f62
Glenn Ergeerts <glenn.ergeerts@uantwerpen.be>
2017-02-06 08:55:47 +0100

    commandline parse tool now can parse ALP commands, serial messages and foreground frames, specify using -t param

--- examples/parse_bytestring.py
+++ examples/parse_bytestring.py
@@ -1,12 +1,30 @@
+import argparse
 import fileinput
 
-from d7a.serial_console_interface.parser import Parser
+from bitstring import ConstBitStream
 
-parser = Parser()
-for hexstring in fileinput.input():
-  hexstring = hexstring.strip().replace(' ', '')
-  cmds, info = parser.parse(bytearray(hexstring.decode("hex")))
-  for cmd in cmds:
-    print cmd
+from d7a.alp.parser import Parser as AlpParser
+from d7a.dll.parser import Parser as ForegroundFrameParser
+from d7a.serial_console_interface.parser import Parser as SerialParser
 
-  print info
+parser_types = ["fg", "alp", "serial"]
+argparser = argparse.ArgumentParser()
+argparser.add_argument("-t", "--type", choices=parser_types, required=True)
+argparser.add_argument('data', help="The data to be parsed, input as an hexstring")
+args = argparser.parse_args()
+
+hexstring = args.data.strip().replace(' ', '')
+data = bytearray(hexstring.decode("hex"))
+if args.type == "alp":
+  print AlpParser().parse(ConstBitStream(data), len(data))
+  exit(0)
+if args.type == "serial":
+  parser = SerialParser()
+if args.type == "fg":
+  parser = ForegroundFrameParser()
+
+cmds, info = parser.parse(data)
+for cmd in cmds:
+  print cmd
+
+print info
