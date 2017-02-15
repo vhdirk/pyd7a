d450355ec3fd10ae003a828ba5291f0648b127e4
Glenn Ergeerts <glenn.ergeerts@uantwerpen.be>
2017-02-15 07:57:50 +0100

    adapted D7ATP control field to spec v1.1

--- d7a/d7atp/parser.py
+++ d7a/d7atp/parser.py
@@ -7,17 +7,8 @@ from d7a.alp.parser import Parser as AlpParser
 class Parser(object):
 
   def parse(self, bitstream, payload_length):
-    is_dialog_start = bitstream.read("bool")
-    is_dialog_end = bitstream.read("bool")
-    _ = bitstream.read("pad:2"),
-    is_ack_return_template_requested = bitstream.read("bool")
-    is_ack_not_void = bitstream.read("bool")
-    is_ack_recorded = bitstream.read("bool")
-    _ = bitstream.read("pad:1")
+    control = Control.parse(bitstream)
     payload_length = payload_length - 1 # subtract control byte
-
-    control = Control(is_dialog_start, is_dialog_end, is_ack_return_template_requested,
-                      is_ack_not_void, is_ack_recorded)
 
     dialog_id = bitstream.read("uint:8")
     payload_length = payload_length - 1
@@ -26,18 +17,18 @@ class Parser(object):
     payload_length = payload_length - 1
 
     ack_template = None
-    if is_ack_not_void:
+    if control.is_ack_not_void:
       transaction_id_start = bitstream.read("uint:8")
       payload_length = payload_length - 1
       transaction_id_stop = bitstream.read("uint:8")
       payload_length = payload_length - 1
-      assert transaction_id_start == transaction_id, "Other case not implemented yet"
-      assert transaction_id_stop == transaction_id, "Other case not implemented yet"
+      # TODO assert transaction_id_start == transaction_id, "Other case not implemented yet"
+      # TODO assert transaction_id_stop == transaction_id, "Other case not implemented yet"
       # TODO ack bitmap (for when transaction_id_start != transaction_id)
       ack_template = [ transaction_id_start, transaction_id_stop ]
 
-    assert is_ack_recorded == False, "Not implemented yet"
-    assert is_ack_not_void == False, "Not implemented yet"
+    assert control.is_ack_record_requested == False, "Not implemented yet"
+    assert control.is_ack_not_void == False, "Not implemented yet"
 
     alp_command = AlpParser().parse(bitstream, payload_length)
 
--- d7a/dll/test/parser.py
+++ d7a/dll/test/parser.py
@@ -22,12 +22,12 @@ class TestParser(unittest.TestCase):
       0x20, # D7ANP control
       0x01,
       0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, # Origin Access ID
-      0xc8, # D7ATP control
+      0xa8, # D7ATP control
       0xe9, # dialog ID
       0x00, # transaction ID
       0x01, # ALP control (read file data operation)
       0x00, 0x00, 0x08, # file data request operand (file ID 0x00)
-      0x73, 0xD3 # CRC
+      0xC0, 0xC9 # CRC
     ]
 
     (frames, info) = self.parser.parse(read_id_command)
@@ -44,11 +44,13 @@ class TestParser(unittest.TestCase):
     self.assertEqual(frame.d7anp_frame.control.origin_id_type, IdType.UID)
     self.assertEqual(frame.d7anp_frame.origin_access_class, 0x01)
     self.assertEqual(frame.d7anp_frame.origin_access_id, [0, 0, 0, 0, 0, 0, 0, 1])
-    self.assertFalse(frame.d7anp_frame.d7atp_frame.control.is_ack_recorded)
-    self.assertTrue(frame.d7anp_frame.d7atp_frame.control.is_ack_return_template_requested)
-    self.assertTrue(frame.d7anp_frame.d7atp_frame.control.is_dialog_end)
     self.assertTrue(frame.d7anp_frame.d7atp_frame.control.is_dialog_start)
+    self.assertTrue(frame.d7anp_frame.d7atp_frame.control.has_tl)
+    self.assertFalse(frame.d7anp_frame.d7atp_frame.control.has_te)
+    self.assertTrue(frame.d7anp_frame.d7atp_frame.control.is_ack_requested)
     self.assertFalse(frame.d7anp_frame.d7atp_frame.control.is_ack_not_void)
+    self.assertFalse(frame.d7anp_frame.d7atp_frame.control.is_ack_record_requested)
+    self.assertFalse(frame.d7anp_frame.d7atp_frame.control.has_agc)
     self.assertEqual(frame.d7anp_frame.d7atp_frame.dialog_id, 0xe9)
     self.assertEqual(frame.d7anp_frame.d7atp_frame.transaction_id, 0)
     self.assertEqual(len(frame.d7anp_frame.d7atp_frame.alp_command.actions), 1)
@@ -71,7 +73,7 @@ class TestParser(unittest.TestCase):
                    0x20,  # D7ANP control
                    0x01, # origin access class
                    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, # origin access ID
-                   0x41,  # D7ATP control
+                   0xa8,  # D7ATP control
                    0xe9,  # dialog ID
                    0x00,  # transaction ID
                    0x20,  # ALP control (return file data operation)
@@ -96,11 +98,13 @@ class TestParser(unittest.TestCase):
     self.assertEqual(frame.d7anp_frame.control.origin_id_type, IdType.UID)
     self.assertEqual(frame.d7anp_frame.origin_access_class, 0x01)
     self.assertEqual(frame.d7anp_frame.origin_access_id, [0, 0, 0, 0, 0, 0, 0, 2])
-    self.assertFalse(frame.d7anp_frame.d7atp_frame.control.is_ack_recorded)
-    self.assertFalse(frame.d7anp_frame.d7atp_frame.control.is_ack_return_template_requested)
-    self.assertTrue(frame.d7anp_frame.d7atp_frame.control.is_dialog_end)
-    self.assertFalse(frame.d7anp_frame.d7atp_frame.control.is_dialog_start)
+    self.assertTrue(frame.d7anp_frame.d7atp_frame.control.is_dialog_start)
+    self.assertTrue(frame.d7anp_frame.d7atp_frame.control.has_tl)
+    self.assertFalse(frame.d7anp_frame.d7atp_frame.control.has_te)
+    self.assertTrue(frame.d7anp_frame.d7atp_frame.control.is_ack_requested)
     self.assertFalse(frame.d7anp_frame.d7atp_frame.control.is_ack_not_void)
+    self.assertFalse(frame.d7anp_frame.d7atp_frame.control.is_ack_record_requested)
+    self.assertFalse(frame.d7anp_frame.d7atp_frame.control.has_agc)
     self.assertEqual(frame.d7anp_frame.d7atp_frame.dialog_id, 0xe9)
     self.assertEqual(frame.d7anp_frame.d7atp_frame.transaction_id, 0)
     self.assertEqual(len(frame.d7anp_frame.d7atp_frame.alp_command.actions), 1)
