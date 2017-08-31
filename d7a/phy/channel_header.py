fa014005a747f30e01f86ee11e91d174bd874bb0
Glenn Ergeerts <glenn.ergeerts@uantwerpen.be>
2017-08-31 07:54:28 +0200

    override equality operator for channel_header

--- d7a/phy/channel_header.py
+++ d7a/phy/channel_header.py
@@ -53,3 +53,13 @@ class ChannelHeader(Validatable):
       self.channel_class,
       self.channel_band
     )
+
+  def __eq__(self, other):
+    if type(other) is type(self):
+      return self.__dict__ == other.__dict__
+    return False
+
+  def __ne__(self, other):
+    if isinstance(other, self.__class__):
+      return not self.__eq__(other)
+    return False
