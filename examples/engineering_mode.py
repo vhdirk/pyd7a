fc9d1bfc6244dea4709d15af91a06aed268c803c
Glenn Ergeerts <glenn.ergeerts@aloxy.io>
2019-03-22 16:45:05 +0100

    set engineering mode of remote node over D7 interface

--- examples/engineering_mode.py
+++ examples/engineering_mode.py
@@ -9,8 +9,12 @@ import logging
 from d7a.alp.command import Command
 from d7a.alp.interface import InterfaceType
 from d7a.d7anp.addressee import Addressee, IdType
+from d7a.dll.access_profile import AccessProfile
+from d7a.dll.sub_profile import SubProfile
+from d7a.phy.subband import SubBand
 from d7a.sp.configuration import Configuration
 from d7a.sp.qos import QoS, ResponseMode
+from d7a.system_files.access_profile import AccessProfileFile
 from d7a.system_files.uid import UidFile
 from d7a.system_files.engineering_mode import EngineeringModeFile
 from d7a.system_files.system_file_ids import SystemFileIds
@@ -42,21 +46,45 @@ configure_default_logger(config.verbose)
 
 modem = Modem(config.device, config.rate, unsolicited_response_received_callback=received_command_callback)
 modem.connect()
-logging.info("Executing query...")
+
 
 emFile = EngineeringModeFile(mode=1, flags=0, timeout=10, channel_header=ChannelHeader(ChannelCoding.PN9,ChannelClass.NORMAL_RATE,ChannelBand.BAND_868), center_freq_index=0, eirp=0)
 print(emFile)
 
 waiting_for_requests += 1
-modem.execute_command_async(
-  # alp_command=Command.create_with_write_file_action_system_file(
-  #   file=emFile,
-  #   interface_type=InterfaceType.HOST
-  # )
+
+channel_index = 128
+access_profile = AccessProfile(
+    channel_header=ChannelHeader(channel_band=ChannelBand.BAND_868,
+                               channel_coding=ChannelCoding.FEC_PN9,
+                               channel_class=ChannelClass.LO_RATE),
+    sub_profiles=[SubProfile(subband_bitmap=0), SubProfile(), SubProfile(),
+                  SubProfile()],
+    sub_bands=[SubBand(
+      channel_index_start=channel_index,
+      channel_index_end=channel_index,
+      eirp=10,
+      cca=86
+    )] * 8
+  )
+
+modem.execute_command(Command.create_with_write_file_action_system_file(
+  file=AccessProfileFile(access_profile=access_profile, access_specifier=0)),
+  timeout_seconds=200
+)
+
+modem.execute_command(
   alp_command=Command.create_with_write_file_action(
     file_id=5,
     data=list(emFile),
-    interface_type=InterfaceType.HOST
+    interface_type=InterfaceType.D7ASP,
+    interface_configuration=Configuration(
+      qos=QoS(resp_mod=ResponseMode.RESP_MODE_ALL),
+      addressee=Addressee(
+        access_class=0x01,
+        id_type=IdType.NOID
+      )
+    )
   )
 )
 
