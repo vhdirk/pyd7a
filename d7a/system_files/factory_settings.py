faefef511c284fb91472190f5fc137b7c32fb19f
Liam Oorts <liam.oorts@aloxy.io>
2019-04-26 16:36:13 +0200

    updated to use Hz instead of predefined values

--- d7a/system_files/factory_settings.py
+++ d7a/system_files/factory_settings.py
@@ -9,33 +9,36 @@ class FactorySettingsFile(File, Validatable):
 
   SCHEMA = [{
     "gain": Types.INTEGER(min=-128, max=127),
-    "rx_bw_low_rate": Types.INTEGER(min=0, max=31),
-    "rx_bw_normal_rate": Types.INTEGER(min=0, max=31),
-    "rx_bw_high_rate": Types.INTEGER(min=0, max=31)
+    "rx_bw_low_rate": Types.INTEGER(min=0, max=0xFFFFFFFF),
+    "rx_bw_normal_rate": Types.INTEGER(min=0, max=0xFFFFFFFF),
+    "rx_bw_high_rate": Types.INTEGER(min=0, max=0xFFFFFFFF)
   }]
 
-  def __init__(self, gain=0, rx_bw_low_rate=0x14, rx_bw_normal_rate=0x11, rx_bw_high_rate=0x01):
+  def __init__(self, gain=0, rx_bw_low_rate=20936, rx_bw_normal_rate=157291, rx_bw_high_rate=251736):
     self.gain = gain
     self.rx_bw_low_rate = rx_bw_low_rate
     self.rx_bw_normal_rate = rx_bw_normal_rate
     self.rx_bw_high_rate = rx_bw_high_rate
-    File.__init__(self, SystemFileIds.FACTORY_SETTINGS, 4)
+    File.__init__(self, SystemFileIds.FACTORY_SETTINGS, 0x0D)
     Validatable.__init__(self)
 
   @staticmethod
   def parse(s):
     gain = s.read("int:8")
-    rx_bw_low_rate = s.read("uint:8")
-    rx_bw_normal_rate = s.read("uint:8")
-    rx_bw_high_rate = s.read("uint:8")
+    rx_bw_low_rate = s.read("uint:32")
+    rx_bw_normal_rate = s.read("uint:32")
+    rx_bw_high_rate = s.read("uint:32")
 
     return FactorySettingsFile(gain=gain, rx_bw_low_rate=rx_bw_low_rate, rx_bw_normal_rate=rx_bw_normal_rate, rx_bw_high_rate=rx_bw_high_rate)
 
   def __iter__(self):
     yield self.gain
-    yield self.rx_bw_low_rate
-    yield self.rx_bw_normal_rate
-    yield self.rx_bw_high_rate
+    for byte in bytearray(struct.pack(">I",self.rx_bw_low_rate)):
+      yield byte
+    for byte in bytearray(struct.pack(">I",self.rx_bw_normal_rate)):
+      yield byte
+    for byte in bytearray(struct.pack(">I",self.rx_bw_high_rate)):
+      yield byte
 
   def __str__(self):
     return "gain={}, rx_bw_low_rate={}, rx_bw_normal_rate={}, rx_bw_high_rate={}".format(self.gain, hex(self.rx_bw_low_rate), hex(self.rx_bw_normal_rate), hex(self.rx_bw_high_rate))
