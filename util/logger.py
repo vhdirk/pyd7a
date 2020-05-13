2b46c09167611e93bfaea7dd4f1d13e65457c790
Liam Oorts <liam.oorts@aloxy.io>
2020-05-13 11:04:05 +0200

    add example to write update to file

--- util/logger.py
+++ util/logger.py
@@ -25,12 +25,12 @@ import logging
 
 # configure the logger when using the supplied examples, as opposed to running as a library
 # called from an external program (which then should take care of configuring the logging)
-def configure_default_logger(is_verbose_enabled):
+def configure_default_logger(is_verbose_enabled, logging_level = logging.INFO):
   log = logging.getLogger()
   formatter = logging.Formatter('%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
   handler = logging.StreamHandler()
   handler.setFormatter(formatter)
   log.addHandler(handler)
-  log.setLevel(logging.INFO)
+  log.setLevel(logging_level)
   if is_verbose_enabled:
     log.setLevel(logging.DEBUG)
